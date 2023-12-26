c8f56c957fead253b4cdcade0708fc553234c92c
Uğur Oğuzhan Karadeniz <uokaradeniz@gmail.com>
2023-12-26 20:03:26 +0300

    dashboard listeler eklendi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,12 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using WebApplicationMVC2.Models;
 
 namespace WebApplicationMVC2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Table()
-        {
-            return View();
-        }
-
         public ActionResult Dashboard()
         {
             return View();
